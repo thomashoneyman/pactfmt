d014bf9791392cdb5317aaf8ff736d9c3190b6bb
Thomas Honeyman <hello@thomashoneyman.com>
2024-07-18 18:53:52 -0400

    Add lexer implementation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
+pub mod lexer;
+
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
 }
