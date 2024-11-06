ae577612598a0dda0e81977f7e2d089d88ab9ab1
Thomas Honeyman <hello@thomashoneyman.com>
2024-11-06 07:17:49 -0500

    Initial lexer implementation (#1)

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod lexer;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Check an input
+    Check {
+        /// The input source
+        input: String,
+    },
+    /// Format an input
+    Format {
+        /// The input source
+        input: String,
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Check { input }) => {
+            println!("Check! {}", input);
+        }
+        Some(Commands::Format { input }) => {
+            println!("{}", input)
+        }
+        _ => {
+            println!("Unrecognized!")
+        }
+    }
+}
